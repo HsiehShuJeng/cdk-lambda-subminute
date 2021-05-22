cdc279526b801ebdb337a47ad1f03248c0213441
scott.hsieh <fantasticSie@hotmail.com>
2021-05-22 21:25:57 +0800

    attempt 2

--- test/integ.test.ts
+++ test/integ.test.ts
@@ -1,5 +1,5 @@
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
 import { SynthUtils } from '@aws-cdk/assert';
 import '@aws-cdk/assert/jest';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
